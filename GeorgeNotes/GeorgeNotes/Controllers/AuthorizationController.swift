b9c0fcb12e35f61f5487a180ab18ef396b4f2e7b
SirGawain <george@georges-Mac.local>
2016-08-21 22:49:16 +0300

    Authorisation Controller

--- GeorgeNotes/GeorgeNotes/Controllers/AuthorizationController.swift
+++ GeorgeNotes/GeorgeNotes/Controllers/AuthorizationController.swift
@@ -10,8 +10,75 @@ import UIKit
 
 class AuthorizationController: UIViewController
 {
+    
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    var tempUser: User? = User("","","","",nil)
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+    }
+    
+    @IBAction func tryEnterClick(sender: AnyObject)
+    {
+        let login = loginField.text!
+        let password = passwordField.text!
+        tempUser = store.findUser(login,password)
+        
+        if tempUser != nil
+        {
+            performSegueWithIdentifier("GoToPersonalPage", sender: nil)
+        }
+        else
+        {
+            MessageBox("Ошибка!","Неверное имя пользователя или пароль.")
+        }
+    }
+
+}
+
+//MARK:- Скрыть клавиатуру
+extension AuthorizationController
+{
+    func hideKeyboardWhenTappedAround()
+    {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(RegistrationController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard()
+    {
+        view.endEditing(true)
+    }
+}
+
+//MARK:- Переходы
+extension AuthorizationController
+{
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        if segue.identifier == "GoToPersonalPage"
+        {
+            let destination = segue.destinationViewController as! PersonalPageController
+            destination.tempUser = tempUser!
+        }
+    }
+}
+
+//MARK:- Сообщение об ошибке
+extension AuthorizationController
+{
+    func MessageBox(title: String, _ message: String)
+    {
+        let messangeBox1 = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "ОК", style: .Default)
+        {
+            (action) in
+        }
+        messangeBox1.addAction(okAction)
+        presentViewController(messangeBox1, animated: true, completion: nil)
     }
 }
--- GeorgeNotes/GeorgeNotes/Controllers/PersonalPageController.swift
+++ GeorgeNotes/GeorgeNotes/Controllers/PersonalPageController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class PersonalPageController: UIViewController
 {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var surnameLabel: UILabel!
+    @IBOutlet weak var imageUser: UIImageView!
+    @IBOutlet weak var BarName: UINavigationItem!
+    
+    var tempUser = User("","","","",nil)
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        nameLabel.text = tempUser.name
+        surnameLabel.text = tempUser.surname
+        imageUser.image = tempUser.image
+        BarName.title = tempUser.login
     }
 }
